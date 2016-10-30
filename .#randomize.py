b897562e43099cccf087e543709cfae6bb17be45
Ubuntu <ubuntu@ip-172-31-0-245.us-west-2.compute.internal>
2016-10-30 03:39:47 +0000

    affine with transparent background

---
+++ .#randomize.py
@@ -0,0 +1,1 @@
+ubuntu@ip-172-31-0-245.us-west-2.compute.internal.1930:1477796066
--- transparency.py
+++ transparency.py
@@ -15,10 +15,10 @@ def createAffineTransformation(img, i):
     rows, cols, ch = img.shape
     
     M = cv2.getAffineTransform(pts1, pts2)
-    dst = cv2.warpAffine(img, M, (cols, rows), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT, borderValue=[0, 0, 0, 0])
+    dst = cv2.warpAffine(img, M, (cols, rows), flags=cv2.INNER_CUBIC, borderMode=BORDER_TRANSPARENT)
     
     filename = "adidas_affine_" + str(i) + ".png"
-    cv2.imwrite(filename, dst)
+    cv2.imwrite(filename, dst, -1)
 
 def medianBlurring(img, i): # i can only be odd!!!!! Its kernel size should be a positive odd integer.
     kernel = np.ones((5,5),np.float32)/25
@@ -41,10 +41,14 @@ def morphologicalTransform(img, i):
 if __name__ == "__main__":
     # load the games image
     print("Starting program")
-    img = cv2.imread("adidas1.png", cv2.IMREAD_UNCHANGED)
+    img = cv2.imread("adidas1.png", -1)
+    b_channel, g_channel, r_channel = cv2.split(img)
+    alpha_channel = b_channel
+    img_RGBA = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
+    print(img_RGBA.shape)
     for i in range(0, 30):
         print("For loop 1 iteration " + str(i))
-        createAffineTransformation(img, i)
+        createAffineTransformation(img_RGBA, i)
     medianBlurring(img, 5)
     laplacianGradient(img, 0)
 #    for i in range(2, 20):
