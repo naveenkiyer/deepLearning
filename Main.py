34ad3c23454abec0a157f11d42ec7d7a32e312ee
Ubuntu <ubuntu@ip-172-31-0-245.us-west-2.compute.internal>
2016-10-30 03:15:21 +0000

    Added main method

--- Main.py
+++ Main.py
@@ -1,1 +1,51 @@
+# import the necessary packages
+import numpy as np
 import cv2
+import random
+
+def createAffineTransformation(img, i):
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+    c = random.randint(1, 100)
+    d = random.randint(1, 100)
+
+    pts1 = np.float32([[a,a],[b,a],[a,b]])
+    pts2 = np.float32([[c,d],[b,a],[d,a]])
+    
+    rows, cols, ch = img.shape
+    
+    M = cv2.getAffineTransform(pts1, pts2)
+    dst = cv2.warpAffine(img, M, (cols, rows))
+    
+    filename = "adidas_affine_" + str(i) + ".png"
+    cv2.imwrite(filename, dst)
+
+def medianBlurring(img, i): # i can only be odd!!!!! Its kernel size should be a positive odd integer.
+    kernel = np.ones((5,5),np.float32)/25
+    dst = cv2.filter2D(img,-1,kernel)
+    blurred = cv2.medianBlur(img, i)
+    filename = "adidas_mblur_" + str(i) + ".png"
+    cv2.imwrite(filename, blurred)
+
+def laplacianGradient(img, i):
+    laplacian = cv2.Laplacian(img, cv2.CV_64F)
+    filename = "adidas_laplac_" + str(i) + ".png"
+    cv2.imwrite(filename, laplacian)
+
+def morphologicalTransform(img, i):
+    kernel = np.ones((i,i), np.uint8)
+    gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+    filename = "adidas_morph_" + str(i) + ".png"
+    cv2.imwrite(filename, gradient)
+
+if __name__ == "__main__":
+    # load the games image
+    img = cv2.imread("adidas1.png")
+    for i in range(0, 30):
+        createAffineTransformation(img, i)
+    medianBlurring(img, 5)
+    laplacianGradient(img, 0)
+    for i in range(2, 20):
+        if i % 2 == 1:
+            morphologicalTransform(img, i)
+            cv2.waitKey(0)
